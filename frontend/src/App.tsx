c9252175161cefaca8f44da3339124c5947a89a1
can-tls <talascan@googlemail.com>
2024-03-10 21:11:42 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './layouts/layout';
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path= "/" element={<Layout></Layout>} />
+        <Route path= "/search" element={<>Search Page</>} />
+        <Route path= "" element={<Navigate to="/" />} />
+      </Routes>  
+    </Router>
+  );
+}
+
+export default App
