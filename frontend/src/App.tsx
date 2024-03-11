fbb16486ebbbeb3024c1375141119eda36e99aa2
can-tls <talascan@googlemail.com>
2024-03-11 19:30:19 +0100

    Header and Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import './index.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './layouts/layout';
+import Layout from './layouts/Layout';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path= "/" element={<Layout></Layout>} />
+        <Route
+          path= "/" 
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          } />
         <Route path= "/search" element={<>Search Page</>} />
         <Route path= "" element={<Navigate to="/" />} />
       </Routes>  
