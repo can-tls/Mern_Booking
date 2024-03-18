2fa9df572a5da17d6b69744d821074f2728412c5
Can Talas <talascan@googlemail.com>
2024-03-19 00:15:47 +0100

    Context created

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,3 +1,5 @@
+import React, { useContext } from "react";
+
 type ToastMessage = {
     message: string;
     type: "SUCCESS" | "ERROR";
@@ -5,4 +7,22 @@ type ToastMessage = {
 
 type AppContext = {
     showToast: (toastMessage: ToastMessage) => void;
+};
+
+const AppContext = React.createContext<AppContext | undefined>(undefined);
+
+export const AppContextProvider = ({children}: {children: React.ReactNode}) => {
+    return (
+        <AppContext.Provider value={{
+            showToast: (toastMessage) => {
+                console.log(toastMessage);            },    
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    return context as AppContext;
 };
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
 
 export type RegisterFormData = {
     firstName: string;
@@ -11,14 +12,16 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+        const {showToast} = useAppContext();
+
         const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
 
         const mutation = useMutation(apiClient.register, {
             onSuccess: () => {
-                console.log("Account created successfully");
+                showToast({message: "Registration successfully", type: "SUCCESS"});
             },
             onError: (error: Error) => {
-                console.log(error.message);
+                showToast({message: error.message, type: "ERROR"});
             }
         });
 
